7e89044ca9840815d2f08293de8440d5101b98df
Ida Hägglund <idajespersen92@gmail.com>
2025-10-16 12:50:39 +0200

    Uppdaterat med kod från Gustav

--- 11_Gitövning/Program.cs
+++ 11_Gitövning/Program.cs
@@ -1,11 +1,45 @@
 ﻿namespace _11_Gitövning
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    using System;
+    using System.Collections.Generic;
+    class Program
         {
-            Console.WriteLine("Våran Git-övning!");
+        static List<string> inventory = new List<string>();
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Välkommen till Fabrikshanteringssystemet!");
 
+            while (true)
+                {
+                Console.WriteLine("\nVälj en åtgärd:");
+                Console.WriteLine("1. Lägg till produkt");
+                Console.WriteLine("2. Visa inventarie");
+                Console.WriteLine("3. Avsluta");
+
+                string val = Console.ReadLine();
+                switch (val)
+                    {
+                    case "1":
+                        LäggTillProdukt();
+                        break;
+                    case "2":
+                        VisaInventarie();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Ogiltigt val. Försök igen.");
+                        break;
+                    }
+                }
+            }
+        static void LäggTillProdukt()
+            {
+            // TODO: Implementera metod för att lägga till produkt
+            }
+        static void VisaInventarie()
+            {
+            // TODO: Implementera metod för att visa inventarie
+            }
         }
     }
-}
