0761aa571ff761c10d0f5d23ab624b646ad9b2f6
Ida Hägglund <idajespersen92@gmail.com>
2025-10-21 14:30:11 +0200

    Commented basic structure for project, based on customer requirements.

---
+++ CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Grupp_5_Bokningssystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här är vårat framtida projekt!");
+        }
+    }
+
+    // IDK what to write here but I guess we can start with some comments on what to implement
+    // Use interface as return type where relevant
+
+    // Create a parent class Room
+    // Implement the IBookable interface in the class
+
+
+    // Create a child class GroupRoom
+    // Implement the IBookable interface in the class
+
+
+    // Create a child class ClassRoom
+    // Implement the IBookable interface in the class
+
+    // Create a IBookable interface with methods for booking and cancelling bookings
+    // Handle start and stop times for bookings
+    // Format date and time appropriately
+    // TimeSpan for duration of booking
+
+    // Create List<T> to manage bookings and rooms
+
+    // Implement operations for filtering and searching rooms based on criteria such as capacity and availability
+
+    // Handle sorting of bookings
+
+    // Implement persistency using a file to store room names
+    // Implement persistency using a file to store developers names
+    // Read from and write to these files as needed
+    // Read when program starts
+
+    // User shall be able to:
+    // - Create new bookings
+    // - List all bookings
+    // - Update an existing booking
+    // - Cancel a booking
+    // - List bookings from a specific year
+    // - List rooms with specific properties (e.g., capacity)
+    // - Ability to make new rooms with error handling for duplicate names
+
+
+
+
+    }
