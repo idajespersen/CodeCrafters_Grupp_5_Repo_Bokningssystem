e2ad953b64c3f3c537cb41fc3ec636cf7a639838
Tove Rosén <tovelinnori@gmail.com>
2025-11-01 23:40:20 +0100

    Made some changes in functionality in SelectionScreen.cs

--- CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/Screens/SelectionScreen.cs
+++ CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/Screens/SelectionScreen.cs
@@ -17,29 +17,64 @@ namespace Grupp_5_Bokningssystem.Screens
 
     public abstract class SelectionScreen : Screen
     {
-        protected readonly int _minChoice;
-        protected readonly int _maxChoice;
         protected readonly InputErrorAction _errorAction;
 
-        protected SelectionScreen(Screen? parent, int minChoice, int maxChoice, InputErrorAction errorAction = InputErrorAction.All) 
+        protected SelectionScreen(Screen? parent, int maxChoice, InputErrorAction errorAction = InputErrorAction.All) 
             : base(parent)
         {
-            if (minChoice > maxChoice)
+            if (maxChoice < 1)
                 throw new ArgumentException("minChoice must be lesser than or equal to maxChoice");
 
-            _minChoice = minChoice;
-            _maxChoice = maxChoice;
+            MaxChoice = maxChoice;
             _errorAction = errorAction;
         }
 
-        protected virtual void InputErrorMessage(Language language)
+        public int MaxChoice
         {
-            if (DisplayLanguage.Selected == Language.Swedish)
+            get;
+        }
+
+        public void HandleInputError()
+        {
+            if ((_errorAction & InputErrorAction.ClearConsole) != 0)
+                Console.Clear();
+
+            DisplayInputErrorMessage(DisplayLanguage.Selected);
+
+            if ((_errorAction & InputErrorAction.AwaitEnterKey) != 0)
+                Console.ReadKey();
+        }
+
+        protected override bool HandleInput(string inputString)
+        {
+            if(!int.TryParse(inputString, out int choice) && (choice < 0 || choice > MaxChoice))
+            {
+                HandleInputError();
+                return false;
+            }
+
+            if(choice > 0)
+            {
+                HandleValidChoice(choice);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Handle a valid choice inbetween 1 and a given maxChoice.
+        /// </summary>
+        /// <param name="choice"></param>
+        public abstract void HandleValidChoice(int choice);
+
+        protected virtual void DisplayInputErrorMessage(Language language)
+        {
+            if (language == Language.Swedish)
             {
                 Console.WriteLine("Felmeddelande:");
                 Console.WriteLine();
                 Console.WriteLine("Du skrev inte en giltig siffra.");
-                Console.WriteLine($"Vänligen skriv in en siffra mellan [{_minChoice}]-[{_maxChoice}].");
+                Console.WriteLine($"Vänligen skriv in en siffra mellan [1]-[{MaxChoice}].");
                 Console.WriteLine();
                 Console.Write("Tryck [ENTER] för att återgå till menyn.");
             }
@@ -48,21 +83,10 @@ namespace Grupp_5_Bokningssystem.Screens
                 Console.WriteLine("Error message:");
                 Console.WriteLine();
                 Console.WriteLine("The given input is invalid.");
-                Console.WriteLine($"Please enter a number between [{_minChoice}]-[{_maxChoice}].");
+                Console.WriteLine($"Please enter a number between [1]-[{MaxChoice}].");
                 Console.WriteLine();
                 Console.Write("Press [ENTER] to return to the menu.");
             }
-        }
-
-        public void DisplayInputError()
-        {
-            if ((_errorAction & InputErrorAction.ClearConsole) != 0)
-                Console.Clear();
-
-            InputErrorMessage(DisplayLanguage.Selected);
-
-            if ((_errorAction & InputErrorAction.AwaitEnterKey) != 0)
-                Console.ReadKey();
-        }
+        }      
     }
 }
