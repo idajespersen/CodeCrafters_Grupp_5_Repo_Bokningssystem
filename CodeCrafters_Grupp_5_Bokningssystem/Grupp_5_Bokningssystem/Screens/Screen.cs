9833ade4ddf4721e541cfead05168462fd2a0e3c
Tove Rosén <tovelinnori@gmail.com>
2025-11-01 22:28:58 +0100

    Created Screen class to handle different type of screens

---
+++ CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/Screens/Screen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_5_Bokningssystem.Screens
+{
+    public abstract class Screen
+    {
+        protected Screen(Screen? parent)
+        {
+            Parent = parent;
+        }
+
+        public Screen? Parent
+        {
+            get;
+        }
+
+        public abstract void DisplayMessage();
+    }
+}
