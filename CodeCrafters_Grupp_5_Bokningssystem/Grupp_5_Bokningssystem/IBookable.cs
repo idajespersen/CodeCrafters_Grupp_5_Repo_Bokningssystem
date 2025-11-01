54ead297d62d35e5d6a12b968cb10c6b9fbfb01f
Tove Rosén <tovelinnori@gmail.com>
2025-11-01 21:27:25 +0100

    Created interface-file IBookable

---
+++ CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/IBookable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_5_Bokningssystem
+{
+    public interface IBookable
+    {
+        void NewBooking();
+
+        void CancelBooking();
+
+        void UpdateBooking();
+
+        void ListBookings();
+
+        void ListBookingsByYear();
+
+        bool IsAvailable(DateTime start, DateTime end);
+
+        // these two make the IBookable more of a IBookableRoom interface
+        //void SearchRoom();
+
+        //void NewRoom();
+    }
+}
