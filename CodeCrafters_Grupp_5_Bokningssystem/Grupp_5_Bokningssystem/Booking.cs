e2d36e2a8f74610760702ab9a912a4ec4b4aefcf
Tove Rosén <tovelinnori@gmail.com>
2025-11-03 17:11:00 +0100

    Added Booking.cs to handle bookings of IBookable

---
+++ CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/Booking.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Grupp_5_Bokningssystem
+{
+    public class Booking
+    {
+        public Booking(IBookable bookable, string madeBy, DateTime startDate, TimeSpan duration)
+        {
+            Bookable = bookable;
+            MadeBy = madeBy;
+            StartDate = startDate;
+            Duration = duration;
+        }
+
+        public IBookable Bookable
+        {
+            get;
+        }
+
+        public string MadeBy
+        {
+            get;
+        }
+
+        public DateTime StartDate
+        {
+            get;
+        }
+
+        public DateTime EndDate
+        {
+            get { return StartDate + Duration; }
+        }
+
+        public TimeSpan Duration
+        {
+            get;
+        }
+    }
+}
