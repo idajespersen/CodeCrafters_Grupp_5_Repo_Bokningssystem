95cd7adf9402e824002bcdf2c23309648e657506
Tove Rosén <tovelinnori@gmail.com>
2025-11-03 16:34:32 +0100

    Added FileHelper.cs to help with loading and saving files.

---
+++ CodeCrafters_Grupp_5_Bokningssystem/Grupp_5_Bokningssystem/FileHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grupp_5_Bokningssystem.Rooms;
+
+namespace Grupp_5_Bokningssystem
+{
+    public static class FileHelper
+    {
+        public static readonly string AppName = "Bokningssystemet";
+        public static readonly string RootPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            AppName);
+        public static readonly string ClassRoomPath = Path.Combine(RootPath, "ClassRooms");
+        public static readonly string GroupRoomPath = Path.Combine(RootPath, "GroupRooms");
+
+        private static void CreateRoomsFolders()
+        {
+            Directory.CreateDirectory(ClassRoomPath);
+            Directory.CreateDirectory(GroupRoomPath);
+        }
+
+        public static List<Room> LoadRooms()
+        {
+            CreateRoomsFolders();
+
+            List<Room> rooms = new List<Room>();
+
+
+
+            return rooms;
+        }
+
+        public static void SaveRooms(List<Room> rooms)
+        {
+            CreateRoomsFolders();
+        }
+    }
+}
